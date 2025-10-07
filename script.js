469097cd5a8b752979969b08aa3f9a7abcd3210b
hmdmsv <mousavi.info@gmail.com>
2025-10-08 01:16:41 +0330

    Initial commit

--- script.js
+++ script.js
@@ -23,14 +23,27 @@ function selectItem(selected) {
 // });
 
 const path = document.getElementById('curve');
+const path1 = document.getElementById('curve1');
 const items = document.querySelectorAll('.item');
+const items1 = document.querySelectorAll('.item1');
 
 const totalLength = path.getTotalLength();
+const totalLength1 = path1.getTotalLength();
 const count = items.length;
+const count1 = items1.length;
 
-// فاصله مساوی بین المان‌ها روی مسیر
+const svgRect = path.ownerSVGElement.getBoundingClientRect();
+
 items.forEach((item, i) => {
   const point = path.getPointAtLength(((i + 0.5) * totalLength) / count);
-  item.style.left = `${point.x}px`;
-  item.style.top = `${point.y}px`;
+  item.style.left = `${point.x + svgRect.left}px`;
+  item.style.top = `${point.y + svgRect.top}px`;
+});
+
+const svgRect1 = path1.ownerSVGElement.getBoundingClientRect();
+
+items1.forEach((item1, i) => {
+  const point1 = path1.getPointAtLength(((i + 0.5) * totalLength1) / count1);
+  item1.style.left = `${point1.x + svgRect1.left}px`;
+  item1.style.top = `${point1.y + svgRect1.top}px`;
 });
